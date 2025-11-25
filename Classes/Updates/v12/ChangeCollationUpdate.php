4925e1c921234c00057d70c0e90b928447a33fd9
Benoit Chenu <bchenu@gaya.fr>
2025-11-25 04:03:49 +0100

    [BUGFIX] Call to undefined method Doctrine\DBAL\Types\IntegerType::getName()
    Revolve: #16

--- Classes/Updates/v12/ChangeCollationUpdate.php
+++ Classes/Updates/v12/ChangeCollationUpdate.php
@@ -120,7 +120,7 @@ final class ChangeCollationUpdate implements UpgradeWizardInterface
 
             // Change each column collation
             foreach ($table->getColumns() as $column) {
-                if ($column->getType()->getName() === 'string' || $column->getType()->getName() === 'text') {
+                if ($column->getType() instanceof StringType || $column->getType() instanceof TextType) {
 
                     $columnCollation = $this->getColumnCollation($connection, $schemaManager, $tableName, $column->getName());
                     if ($columnCollation === $this->collate) {
